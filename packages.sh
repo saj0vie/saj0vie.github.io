8e5f86d0674594c51c2b7c6f02e6879877625d9c
saj0vie <saj0viedev@gmail.com>
2018-07-08 10:29:53 -0400

    Package Release
    hideUpdateDots Release

--- packages.sh
+++ packages.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-dpkg-deb -bZgzip projects/wifiFirmwareUpdater debs
-dpkg-deb -bZgzip projects/sendTestBulletin debs
-dpkg-deb -bZgzip projects/Android7.0Lockdroid debs
-dpkg-deb -bZgzip projects/foldersInFolders debs
-dpkg-deb -bZgzip projects/StillClock debs
-dpkg-deb -bZgzip projects/ClearSwitcher debs
+dpkg-deb -bZgzip projects/com.saj0vie.hideupdatedots debs
 # dpkg-deb -bZgzip projects/<project name> <output folder>
 
